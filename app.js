abbd000e5a6aea952cf0fe32e0d2ce5562441ad8
HoRRoR <HoRRoR@gmail.de>
2017-03-12 18:40:30 +0100

    [FEHLER] fixed ranking in details & random

--- app.js
+++ app.js
@@ -22,9 +22,9 @@ app.controller("myCtrl", function($scope, $location, $routeParams, $filter) {
 
   $scope.word = {};
   $scope.sortType = "-voting";
+  $scope.showVoteMsg = 0;
   $scope.curPage = 0;
   $scope.pageSize = 10;
-  $scope.showMsg = 0;
 
   $scope.savedWords = localStorage.getItem('words');
   var words = (localStorage.getItem('words')!==null) ? JSON.parse($scope.savedWords) : [];
@@ -33,10 +33,11 @@ app.controller("myCtrl", function($scope, $location, $routeParams, $filter) {
 
   $scope.id = (localStorage.getItem('id')!==null) ? localStorage.getItem('id') : 0;
 
+
   $scope.$on('$routeChangeSuccess', function() {
-    var selWord = $routeParams.id;
-    $scope.selWord = parseInt(selWord, 10);
-    $scope.selWord = $scope.words.indexOf($filter('filter')($scope.words, {id: $scope.selWord}, true)[0]);
+    var id_selectedWord = $routeParams.id;
+    $scope.id_selectedWord = parseInt(id_selectedWord, 10);
+    $scope.index_selectedWord = $scope.words.indexOf($filter('filter')($scope.words, {id: $scope.id_selectedWord}, true)[0]);
   }); 
 
   $scope.numberOfPages = function() {
@@ -52,15 +53,10 @@ app.controller("myCtrl", function($scope, $location, $routeParams, $filter) {
     return ($scope.sortType == sortType);
   };
 
-  $scope.saveOrdered = function(){
+  $scope.saveWord = function(){
     $scope.words = $filter('orderBy')($scope.words, '-voting');
     localStorage.setItem('words', JSON.stringify($scope.words));
-    localStorage.setItem('id', JSON.stringify($scope.id));
     $scope.words = JSON.parse(localStorage.getItem('words'));
-  };
-
-  $scope.save = function(){
-    localStorage.setItem('words', JSON.stringify($scope.words));
   };
 
   $scope.addWord  = function (){
@@ -68,12 +64,12 @@ app.controller("myCtrl", function($scope, $location, $routeParams, $filter) {
       return;
     }
     else {
-
       var timestamp = new Date();
       $scope.words.push({id:$scope.id++, name:$scope.word.name, voting:0, date:timestamp});
       $scope.word.name = "";
+      $scope.saveWord();
+      localStorage.setItem('id', JSON.stringify($scope.id));
     }
-    $scope.saveOrdered();
   };
 
   $scope.decreaseVoting = function (index) {
@@ -89,13 +85,19 @@ app.controller("myCtrl", function($scope, $location, $routeParams, $filter) {
     $scope.curPage = 0;
   };
 
-  $scope.showDetails = function(id) {
-    $scope.selWord = $scope.words.indexOf($filter('filter')($scope.words, {id: id}, true)[0]);
+  $scope.findSelWordIndex = function() {
+    $scope.index_selectedWord = $scope.words.indexOf($filter('filter')($scope.words, {id: $scope.id_selectedWord}, true)[0]);
+  };
+
+  $scope.findRndWordIndex = function() {
+    $scope.index_randomWord = $scope.words.indexOf($filter('filter')($scope.words, {id: $scope.id_randomWord}, true)[0]);
   };
 
 
   $scope.randomWord = function() {
     $scope.random = Math.floor(Math.random() * $scope.words.length);
+    $scope.id_randomWord = $scope.words[$scope.random].id;
+    $scope.findRndWordIndex();
   };
 });
 
